453f615abb6b92f71e145de04c6b35dcacfbaa40
num <de0811@naver.com>
2025-05-04 16:27:13 +0900

    basePath와 assetPrefix 추가

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  basePath: "/github-blog",
+  assetPrefix: "/github-blog",
 };
 
 export default nextConfig;
