4a029a0fb266894e21e0a932524da6f453839818
num <de0811@naver.com>
2025-05-06 19:37:33 +0900

    대략적인 구조 작업 next.config.ts : 정적 페이지기 때문에 이미지 최적화하지 않도록 수정

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  // basePath: "/github-blog",
-  // assetPrefix: "/github-blog",
+  basePath: "/github-blog",
+  assetPrefix: "/github-blog",
   images: {
     unoptimized: true,
   },
